12a6227704ba0187d2a926840d8c6cfe7db8dc1d
Ma-a-sc <mark.scharmann.ms@gmail.com>
2021-03-22 14:03:18 +0100

    fixes

--- PwManager.py
+++ PwManager.py
@@ -7,36 +7,36 @@ import encryption_functions
 # Function that checks which command the user would like to perform using if
 # statements. The function will be called again if the user did not want to exit
 # after the command was performed.
-def user_inputs():
+def user_inputs(fernet_key, text_file_dictionary):
     command = input("""
-    What action would you like to perform?\n
-    Actions: append, get pw, erase acc and pw, change pw, erase all pws,
-    change masterpassword, exit\n> 
-    """)
+What action would you like to perform?
+Actions: append, get pw, erase acc and pw, change pw, erase all pws,
+change masterpassword, exit
+>""")
 
     if command == 'append':
         pwManager_c.append(fernet_key, text_file_dictionary)
-        user_inputs()
+        user_inputs(fernet_key, text_file_dictionary)
     
     elif command == 'get pw':
         pwManager_c.get_pw(fernet_key, text_file_dictionary)
-        user_inputs()
+        user_inputs(fernet_key, text_file_dictionary)
         
     elif command == 'erase acc and pw':
         pwManager_c.erase_acc_pw(text_file_dictionary)
-        user_inputs()
+        user_inputs(fernet_key, text_file_dictionary)
 
     elif command == 'change pw':
         pwManager_c.change_pw(fernet_key, text_file_dictionary)
-        user_inputs()
+        user_inputs(fernet_key, text_file_dictionary)
 
     elif command == 'erase all pws':
         pwManager_c.erase_all_pws(text_file_dictionary)
-        user_inputs()
+        user_inputs(fernet_key, text_file_dictionary)
 
     elif command == 'change masterpassword':
         pwManager_c.new_master_pw()
-        user_inputs()
+        user_inputs(fernet_key, text_file_dictionary)
 
     elif command == 'exit':
         exit()
@@ -47,7 +47,7 @@ def user_inputs():
         if ex == "yes":
             exit()
         elif ex == "no":
-            user_inputs()
+            user_inputs(fernet_key, text_file_dictionary)
         else:
             print("Invalid, will exit now")
             exit()
@@ -78,7 +78,7 @@ def verification():
             # variable contents as the argument.
             text_file_dictionary = json.loads(contents)
             
-            user_inputs()
+            user_inputs(fernet_key, text_file_dictionary)
 
         else:
             attempts_to_log_in += 1
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from sys import exit
 import json
 import dict_functions
 import encryption_functions
+from cryptography.fernet import Fernet
 
 # Creates the two files pw.txt and masterpw.txt.
 pw_file = open("pw.txt", "w+")
@@ -9,24 +10,25 @@ master_file = open("masterpw.txt", "w+")
 
 # Warns the user to not use this file again if he allready did the setup.
 print("""
-    Thank you for using pwManager. First you need to set a Masterpassword
-    and we need to generate a Fernet key. Fernet is the encryption method
-    used for the passwords. 
-    DO NOT PROGRESS FURTHER IF YOU ALLREADY DID THE SETUP ONCE!
-    """)
+Thank you for using pwManager. First you need to set a Masterpassword
+and we need to generate a Fernet key. Fernet is the encryption method
+used for the passwords. 
+DO NOT PROGRESS FURTHER IF YOU ALLREADY DID THE SETUP ONCE!
+""")
 # Asks if the user wants to progress further.
-progress = input("Do you want to progress furhter?\n yes or no\n>")
+progress = input("Do you want to progress furhter?\nyes or no\n>")
 
 if progress == "yes":
 
-    masterpassword = input(""""
-                Pls set a masterpassword.\n CAUTION: Do not lose it and store it
-                somewhere safe. A usbstick would be good because you also need
-                to store your fernet key somewhere safe.
-                """)
+    masterpassword = input("""
+Pls set a masterpassword.
+CAUTION: Do not lose it and store it
+somewhere safe. A usbstick would be good because you also need
+to store your fernet key somewhere safe.
+""")
     # The user is asked to set a masterpassword. Afterwards the new_master_password
     # function is called to create the hash.
-    new_master_password(masterpassword)
+    dict_functions.new_master_password(masterpassword)
     print(f"You set your masterpassword as: {masterpassword}")
 
     # Generates the Fernet key which is needed for the encryption of the 
@@ -34,13 +36,16 @@ if progress == "yes":
     fernet_key = Fernet.generate_key()
     
     print(f"""
-    Your Fernet key is: {fernet_key}\n Pls store it offline and not on the
-    same pc as the passwordmanager itself. Security is compromised otherwise.
-    """)
+Your Fernet key is: {fernet_key}
+Pls store it offline and not on the same pc as the passwordmanager itself. 
+Security is compromised otherwise.
+""")
 
     set_password = input("""
-    Do you want to set some accounts and passwords?\nOptions: yes or no.\n>
-    """)
+Do you want to set some accounts and passwords?
+Options: yes or no.
+>
+""")
 
     # Opens the pw.txt and writes two brakets into it. This is needed because
     # otherwise json doesnt recognize an dictionary.
@@ -62,7 +67,7 @@ if progress == "yes":
             password = input("Pls input the password.\n>")
             # Calls the function password_to_encrypt which uses Fernet 
             # to encrypt the password given by the user.
-            encrypted_password = password_to_encrypt(password)
+            encrypted_password = encryption_functions.input_to_encrypt(password, fernet_key)
             
             # Simple key value pair with the account and the encrypted password
             # given by the user. Then we update the text_file_dictionary with
@@ -73,10 +78,10 @@ if progress == "yes":
             # The function new_dict is called with the updated text_file_dictionary
             # as the argument. The function writes the updated dictionary into
             # the pw.txt file.
-            new_dict(text_file_dictionary)
+            dict_functions.new_dict(text_file_dictionary)
 
             # Asks the user if he wants to add more and tests the answer.
-            con_ = input("Do you want to add more?\n yes or no\n>")
+            con_ = input("Do you want to add more?\nyes or no\n>")
 
             if con_ == "yes":
                 continue
